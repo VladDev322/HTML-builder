9270b63ab38aaafeccad8da070a332ce72fbd573
VladDev322 <VladL322@yandex.ru>
2023-05-10 06:35:11 +0300

    feat: finish fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) return console.error(err);
+});
+
+const oldFolder = path.join(__dirname, 'files');
+const newFolder = path.join(__dirname, 'files-copy');
+
+fs.promises.readdir(oldFolder)
+  .then(files => {
+    files.forEach(file => {
+      let filePath =  path.join(oldFolder, file);
+      let fileDir = path.join(newFolder, file);
+      fs.copyFile(filePath, fileDir, (err) => {
+        if (err) return console.log(err);
+      });
+    });
+  });
 
